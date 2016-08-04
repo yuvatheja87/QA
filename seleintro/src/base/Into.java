d5e9421ccb669129865dbd05ef9ec9f2e16ee148
yuvatheja <yuvatheja.s@gmail.com>
2016-08-04 17:40:49 -0500

    trail

---
+++ seleintro/src/base/Into.java
@@ -0,0 +1,12 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Into {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+	}
+
+}
