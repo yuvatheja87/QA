21fae63ab841af5fa0ecef84ee151a2f0a00051f
yuvatheja <yuvatheja.s@gmail.com>
2016-08-04 18:40:01 -0500

    trail2

--- seleintro/src/base/Intro.java
+++ seleintro/src/base/Intro.java
@@ -8,11 +8,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Intro {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
-		driver.get("http://www.google.com");
-		System.setProperty("webdriver.chrome.driver","E:\\QA\\workspace\\Selenium-Web"
-				+ "\\Chrome\\chromedriver.exe");
+		driver.get("http://www.google.com"); 			//to open url
+/*		System.setProperty("webdriver.chrome.driver","E:\\QA\\workspace\\Selenium-Web"
+  			+"\\Chrome\\chromedriver.exe");				// to work with chrome
 		WebDriver driver1= new ChromeDriver();
 		driver1.get("http://www.google.com");
+*/		System.out.println(driver.getTitle());   		// to get title of url
+		System.out.println(driver.getPageSource());  	//to get page source
 	}
 }
 	
