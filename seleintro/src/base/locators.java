0ee6bd77a875b448697b8efab2ec83b918914bfa
yuvatheja87 <yuvatheja87@gmail.com>
2016-08-08 16:01:45 -0500

    intro

--- seleintro/src/base/locators.java
+++ seleintro/src/base/locators.java
@@ -1,5 +1,6 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -8,6 +9,11 @@ public class locators {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://facebook.com");
+		driver.findElement(By.id("email")).sendKeys("rpcnaidu@gmail.com");
+		driver.findElement(By.name("pass")).sendKeys("9440044303");
+		driver.findElement(By.xpath(".//*[@id='u_0_o']")).click();
+		
+		
 	}
 
 }
