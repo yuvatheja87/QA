a1da32a575829ef7a37a6f0ecb4504960b737ca7
yuvatheja87 <yuvatheja87@gmail.com>
2016-08-09 11:54:34 -0500

    customized xpath

---
+++ seleintro/src/base/Formsubmit.java
@@ -0,0 +1,13 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Formsubmit {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("");
+	}
+
+}
