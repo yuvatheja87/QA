f5d9f7b5ace5f04c860dd53bd467db2392cb3aeb
yuvatheja87 <yuvatheja87@gmail.com>
2016-08-09 11:06:32 -0500

    customized xpath

--- seleintro/src/base/customizedxpath.java
+++ seleintro/src/base/customizedxpath.java
@@ -1,28 +1,21 @@
 package base;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class locators {
-
+public class customizedxpath {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://facebook.com");
-		driver.findElement(By.id("email")).sendKeys("xxxxxxxxxxxxx");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("xxxxxxxxxxxx");
 //		driver.findElement(By.className("inputText")).sendKeys("xxxxxxx@xxxx.xxx");
-		driver.findElement(By.name("pass")).sendKeys("xxxxxxxxxxxx");
+		driver.findElement(By.xpath("//input[@name='pass']")).sendKeys("xxxxxxxx");
 //		driver.findElement(By.linkText("Forgot account?")).click();
 		driver.findElement(By.partialLinkText("Forgot")).click();
-//		driver.findElement(By.xpath(".//*[@id='u_0_o']")).click();
-//		List<WebElement> l1=(List<WebElement>) driver.findElement(By.tagName("a"));
-//		System.out.println(l1.size());
+
 		
 		
 		
 	}
-
 }
